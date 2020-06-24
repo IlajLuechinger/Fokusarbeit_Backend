d995371666f5ceb068e36359c940c670df05d0a6
Ilaj Luechinger <ilaj.luechinger@ksb-sg.ch>
2020-06-24 14:38:53 +0200

    File

---
+++ src/main/java/org/acme/quarkus/sample/Login.java
@@ -0,0 +1,33 @@
+package org.acme.quarkus.sample;
+
+
+public class Login {
+
+    private String password;
+    private String email;
+
+    public Login(){}
+
+    public Login(String password, String email){
+        this.password = password;
+        this.email = email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+
+}
